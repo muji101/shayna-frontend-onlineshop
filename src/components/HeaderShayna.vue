af47fcd565628390d1023bc55ade9f7146b1804d
muji101 <mujikuwat91@gmail.com>
2021-08-26 21:35:49 +0700

    first commit

---
+++ src/components/HeaderShayna.vue
@@ -0,0 +1,119 @@
+<template>
+        <!-- Header Section Begin -->
+    <header class="header-section">
+        <div class="header-top">
+            <div class="container">
+                <div class="ht-left">
+                    <div class="mail-service">
+                        <i class=" fa fa-envelope"></i> hello.shayna@gmail.com
+                    </div>
+                    <div class="phone-service">
+                        <i class=" fa fa-phone"></i> +628 22081996
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="inner-header">
+                <div class="row">
+                    <div class="col-lg-2 col-md-2">
+                        <div class="logo">
+                            <a href="#">
+                                <router-link to="/">
+                                    <img src="img/logo_website_shayna.png" alt="" />
+                                </router-link>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-7 col-md-7"></div>
+                    <div class="col-lg-3 text-right col-md-3">
+                        <ul class="nav-right">
+                            <li class="cart-icon">
+                                Keranjang Belanja &nbsp;
+                                <a href="#">
+                                    <i class="icon_bag_alt"></i>
+                                    <span>{{ keranjangUser.length }}</span>
+                                </a>
+                                <div class="cart-hover">
+                                    <div class="select-items">
+                                        <table>
+                                            <tbody v-if="keranjangUser.length > 0">
+                                                <tr v-for="keranjang in keranjangUser" :key="keranjang.id">
+                                                    <td class="si-pic">
+                                                        <img class="photo-item" :src="keranjang.photo" alt="" />
+                                                    </td>
+                                                    <td class="si-text">
+                                                        <div class="product-selected">
+                                                            <p>${{ keranjang.price }} x 1</p>
+                                                            <h6>{{ keranjang.name }}</h6>
+                                                        </div>
+                                                    </td>
+                                                    <td @click="removeItem(keranjangUser.index)" class="si-close">
+                                                        <i class="ti-close"></i>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                            <tbody v-else>
+                                                <td>Keranjang kosong</td>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <div class="select-total">
+                                        <span>total:</span>
+                                        <h5>${{ totalHarga }}.00</h5>
+                                    </div>
+                                    <div class="select-button">
+                                        <a href="#" class="primary-btn view-card"><router-link to="/cart" style=" color: #FFF;">VIEW CARD</router-link></a>
+                                        <a href="#" class="primary-btn checkout-btn">CHECK OUT</a>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Header End -->
+</template>
+
+<script>
+export default {
+    name: 'HeaderShayna',
+    data() {
+        return {
+            keranjangUser: []
+        } 
+    },
+    methods: {
+        removeItem(index){
+            this.keranjangUser.splice(index, 1);
+            const parsed = JSON.stringify(this.keranjangUser);
+            localStorage.setItem('keranjangUser', parsed);
+        }
+    },
+    mounted(){
+        if (localStorage.getItem('keranjangUser')) {
+            try {
+                this.keranjangUser = JSON.parse(localStorage.getItem('keranjangUser'));
+            } catch(e) {
+                localStorage.removeItem('keranjangUser');
+            }
+        }
+    },
+    computed: {
+        totalHarga(){
+            return this.keranjangUser.reduce(function(items, data){
+                return items + data.price;
+            }, 0);
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .photo-item {
+        width: 80px;
+        height: 80px;
+    }
+</style>
---
+++ src/components/WomanShayna.vue
@@ -0,0 +1,91 @@
+<template>
+    <!-- Women Banner Section Begin -->
+    <section class="women-banner spad">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12 mt-5" v-if="products.length > 0">
+                    <carousel class="product-slider" :items="3" :nav="false" :autoplay="true" :dots="false">
+                        <div class="product-item" v-for="itemProduct in products" :key="itemProduct.id">
+                            <div class="pi-pic">
+                                <img :src="itemProduct.galleries[0].photo" alt="" />
+                                <ul>
+                                    <li class="w-icon active" @click="saveKeranjang(itemProduct.id, itemProduct.name, itemProduct.price, itemProduct.galleries[0].photo)">
+                                        <a href="#"><i class="icon_bag_alt"></i></a>
+                                    </li>
+                                    <li class="quick-view">
+                                        <router-link :to="'/product/'+itemProduct.id">
+                                            + Quick View
+                                        </router-link>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="pi-text">
+                                <div class="catagory-name">{{ itemProduct.type }}</div>
+                                <router-link to="/product">
+                                        <h5>{{ itemProduct.name }}</h5>
+                                </router-link>
+                                <div class="product-price">
+                                    ${{ itemProduct.price }}
+                                    <span>$35.00</span>
+                                </div>
+                            </div>
+                        </div>
+                    </carousel>
+                </div>
+                <div class="col-lg-12" v-else>
+                    <p>Produk terbaru belum tersedia untuk saat ini.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Women Banner Section End -->
+</template>
+
+<script>
+    import carousel from "vue-owl-carousel";
+    import axios from "axios";
+
+    export default {
+        name: "WomanShayna",
+        components: {
+            carousel
+        },
+        data() {
+            return {
+                products: [],
+                keranjangUser: []
+            };
+        },
+        methods: {
+            saveKeranjang(idProduct, nameProduct, priceProduct, photoProduct){
+                var productStored = {
+                    'id': idProduct,
+                    'name':nameProduct,
+                    'price': priceProduct,
+                    'photo': photoProduct
+                }
+                
+                this.keranjangUser.push(productStored);
+                const parsed = JSON.stringify(this.keranjangUser);
+                localStorage.setItem('keranjangUser', parsed);
+            }
+        },
+        mounted(){
+            axios
+            .get("http://shayna-backend.belajarkoding.com/api/products")
+            // .get("http://127.0.0.1:8000/api/products")
+            .then(res => (this.products = res.data.data.data))
+            .catch(err => console.log(err));
+        }
+    };
+</script>
+
+<style scoped>
+    .product-item {
+        margin-right: 25px;
+    }
+
+    .pi-pic img {
+        max-height: 550px; 
+    }
+</style>
